a9b7d25a7c1c3bd9277f5e5366c39532be562f40
Alex <alex@pixelkraft.net>
2018-06-07 06:18:25 -0400

    User API Routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://alex:Aralogan1!@ds247690.mlab.com:47690/note"
+  mongoURI: "mongodb://alex:Aralogan1!@ds247690.mlab.com:47690/note",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const notes = require("./routes/api/notes");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,6 +22,12 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hello"));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
